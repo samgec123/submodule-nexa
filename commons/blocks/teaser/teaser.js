cefdcf619a2f3303aa3d261cd92e00508a916ebe
Sameer Bhalerao <sbhalera@adobe.com>
2025-02-04 19:14:00 +0530

    remove commons as subtree

--- commons/blocks/teaser/teaser.js
+++
@@ -1,6 +0,0 @@
-// teaser.js
-import teaser from '../../utility/teaserUtils.js';
-
-export default function decorate(block) {
-  teaser.getTeaser(block);
-}
--- commons/utility/apiConstants.js
+++
@@ -1,5 +0,0 @@
-const apiUrls = {
-  sdkUrl: 'https://cf.marutisuzukisubscribe.com/customer/api/v1/sdk-details',
-};
-
-export default apiUrls;
